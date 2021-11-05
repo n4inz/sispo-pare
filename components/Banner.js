eed46cfac47bc09256940015193084e11eeb1a4c
n4inz <nainmo@ymail.com>
2021-11-05 13:48:48 +0800

    Percoba'an splide Pertama

--- components/Banner.js
+++ components/Banner.js
@@ -2,33 +2,33 @@ import Image from "next/image";
 import Upacara from "../public/dummy/upacara.jpg";
 import Lomba from "../public/dummy/lomba.jpg";
 import Pawai from "../public/dummy/pawai.jpg";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
+
 function Banner() {
+  
     return (
-        <div className="flex p-5">
-            <div className="max-w-2xl">
-                <Image 
-                    src={Upacara}
-                    
-                    objectFit="contain"
-                />
-            </div>
-            <div className="flex max-w-xl ">
-                <div className="mx-3 w-24">
-                    <Image 
-                        src={Lomba}
-                        
-                        objectFit="contain"
-                    />
-                </div>
-                <div className="">
-                    <Image 
-                        src={Pawai}
-                        
-                        objectFit="contain"
-                    />
-                </div>
-            </div>
-        </div>
+        <>
+            <Splide
+              options={ {
+                rewind: true,
+                gap   : '1rem',
+                width : '30%',
+                heightRatio : 0.3,
+                autoplay: true
+              } }
+            >
+              <SplideSlide>
+                <img src="dummy/upacara.jpg" alt="Image 1"/>
+              </SplideSlide>
+              <SplideSlide>
+                <img src="dummy/lomba.jpg" alt="Image 2"/>
+              </SplideSlide>
+              <SplideSlide>
+                <img src="dummy/pawai.jpg" alt="Image 3"/>
+              </SplideSlide>
+          </Splide>
+        </>
     )
 }
 
