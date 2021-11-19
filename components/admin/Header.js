11120a599433a743317ada2513feebc43af7234e
n4inz <nainmo@ymail.com>
2021-11-19 13:55:53 +0800

    Percobaan SideBar 2

--- components/admin/Header.js
+++ components/admin/Header.js
@@ -4,34 +4,29 @@ import {
 import {
     MenuIcon
 } from "@heroicons/react/solid";
-import { useState } from "react";
-function Header({Sidebar}) {
-    const [sidebar, setSidebar] = useState(true)
 
-    const togle = () => {
-        setSidebar(!sidebar) 
-        
-    }
-    console.log("header",sidebar)
-
-    Sidebar(sidebar);
+import {RootContext} from "../../context";
+function Header() {
 
     return (
+            <RootContext.Consumer>
+                {
+                    value => {
+                        
+                        return (
+                            <div className="bg-gray-50 flex  border-b-[1px]  left-0 right-0 ">
+                            <div className="flex items-center space-x-4 p-4 ">
+                                <MenuIcon className="h-5 w-5 text-gray-600 hover:cursor-pointer"  onClick={() => value.disPatch()} />
+                                <div className="text-sm text-gray-400">Home</div>
+                            </div>
+    
+                            </div>
+                        )
+                    }
+                }
+            </RootContext.Consumer>
 
-            <div className="bg-gray-50 flex  border-b-[1px]  left-0 right-0 ">
-                <div className="flex items-center space-x-4 p-4 w-1/5">
-                    <div>
-                        <MenuIcon className="h-5 w-5 text-gray-600 hover:cursor-pointer"  onClick={togle} />
-                    </div>
-                    <div className="text-sm text-gray-400">Home</div>
-                </div>
-                <div className={`${sidebar ? "flex items-center space-x-4 p-4 w-4/5" : "hidden"}`}>
-                    <div>
-                        <MenuIcon className="h-5 w-5 text-gray-600 hover:cursor-pointer"  onClick={togle} />
-                    </div>
-                    <div className="text-sm text-gray-400">Home</div>
-                </div>
-            </div>
+
     
     )
 }
