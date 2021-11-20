1fc17820780498cdd0eb7ce1ef41b2b6ee028a99
n4inz <nainmo@ymail.com>
2021-11-20 14:06:10 +0800

    Sideber Ok

--- components/admin/Header.js
+++ components/admin/Header.js
@@ -15,10 +15,19 @@ function Header() {
                         
                         return (
                             <div className="bg-gray-50 flex  border-b-[1px]  left-0 right-0 ">
-                            <div className="flex items-center space-x-4 p-4 ">
-                                <MenuIcon className="h-5 w-5 text-gray-600 hover:cursor-pointer"  onClick={() => value.disPatch()} />
-                                <div className="text-sm text-gray-400">Home</div>
-                            </div>
+                                <div className="flex items-center space-x-4 p-4 w-1/5">
+                                    <div>
+                                        <MenuIcon className="h-5 w-5 text-gray-600 hover:cursor-pointer" onClick={() => value.disPatch()} />
+                                        </div>
+                                        <div className="text-sm text-gray-400">Home</div>
+                                    </div>
+                                    <div className={`${value.togle ? "flex items-center space-x-4 p-4 w-4/5" : "hidden"}`}>
+                                        <div>
+                                            <MenuIcon className="h-5 w-5 text-gray-600 hover:cursor-pointer"  onClick={() => value.disPatch()} />
+                                        </div>
+                                    <div className="text-sm text-gray-400">Home</div>
+                                </div>
+                                
     
                             </div>
                         )
